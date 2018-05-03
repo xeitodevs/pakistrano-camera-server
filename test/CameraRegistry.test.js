4fb651d74b57fe81ef068241abfe473405375dd3
eloylp <e.lopez@xeitodevs.com>
2018-05-03 19:43:46 +0200

    improving testing infrastructure.

--- test/CameraRegistry.test.js
+++ test/CameraRegistry.test.js
@@ -9,19 +9,19 @@ test.beforeEach(() => {
   registry = new CameraRegistry()
 })
 
-test('Registry must add cameras without error', (t) => {
+test('Registry must add cameras without error unit test', (t) => {
   registry.addCamera(livingRoomCamera)
   t.is(registry.registry.length, 1)
 })
 
-test('Registry must find by name previously saved camera', (t) => {
+test('Registry must find by name previously saved camera unit test', (t) => {
   registry.addCamera(livingRoomCamera)
   registry.addCamera(door1Camera)
   const searchResult = registry.findCamera('door1')
   t.deepEqual(searchResult, door1Camera)
 })
 
-test('Registry must throw an exception when camera not found', (t) => {
+test('Registry must throw an exception when camera not found unit test', (t) => {
   const wantedCamera = 'door1'
   const exception = t.throws(() => {
     registry.findCamera(wantedCamera)
