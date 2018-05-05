0668ddf8327b62015e764458b47f3eb4d31587f8
eloylp <e.lopez@xeitodevs.com>
2018-05-06 00:03:04 +0200

    Adding more functions and error handling for camera repository.

--- src/cameraRepository.js
+++ src/cameraRepository.js
@@ -3,6 +3,7 @@
 const config = require('./../config')
 const uuidGen = require('uuid/v4')
 const sqlite = require('sqlite')
+const { DuplicatedCameraException } = require('./DuplicatedCameraException')
 const { CameraNotFoundException } = require('./CameraNotFoundException')
 const connection = sqlite.open(config.database, { Promise })
 const cameraTable = 'camera'
@@ -16,16 +17,32 @@ async function retrieveCamera (name) {
   const db = await connection
   const result = await db.get(`SELECT uuid, name, host, user, password, created_at AS createdAt, updated_at AS updatedAt FROM ${cameraTable} WHERE name=?`, [name])
   if (!result) {
-    throw new CameraNotFoundException()
+    throw new CameraNotFoundException(name)
   }
   return result
 }
 
 async function saveCamera ({ name, host, user, password }) {
+
+  try {
+    const db = await connection
+    const uuid = uuidGen()
+    await db.run(`INSERT INTO ${cameraTable} (uuid, name, host, user, password, created_at) VALUES (?, ?, ?, ?, ?, ?)`, [uuid, name, host, user, password, new Date()])
+    return uuid
+  } catch (e) {
+    if (e.errno === 19) {
+      throw new DuplicatedCameraException(name)
+
+    }
+  }
+}
+
+async function deleteCamera (name) {
   const db = await connection
-  const uuid = uuidGen()
-  await db.run(`INSERT INTO ${cameraTable} (uuid, name, host, user, password, created_at) VALUES (?, ?, ?, ?, ?, ?)`, [uuid, name, host, user, password, new Date()])
-  return uuid
+  const result = await db.run(`DELETE FROM ${cameraTable} WHERE name='${name}'`)
+  if (result.changes !== 1) {
+    throw new CameraNotFoundException(name)
+  }
 }
 
 async function deleteCameras () {
@@ -37,5 +54,6 @@ module.exports = {
   getCameraListing,
   retrieveCamera,
   saveCamera,
+  deleteCamera,
   deleteCameras
 }
--- test/cameraRepository.test.js
+++ test/cameraRepository.test.js
@@ -1,11 +1,14 @@
 const test = require('ava')
+const { CameraDuplicatedException } = require('../src/DuplicatedCameraException')
+const { CameraNotFoundException } = require('../src/CameraNotFoundException')
 const { door1Camera, livingRoomCamera } = require('./resources/fixtures')
-const { assertNearDate } = require('./resources/helpers')
+const { assertNearDate, assertUuid} = require('./resources/helpers')
 
 const {
   getCameraListing,
   retrieveCamera,
   saveCamera,
+  deleteCamera,
   deleteCameras
 } = require('../src/cameraRepository')
 
@@ -19,18 +22,29 @@ test.serial('Get camera must retrieve an specific camera in repository', async (
   const result = await retrieveCamera(livingRoomCamera.name)
   t.is(result.uuid, cam2Uuid)
 })
+test.serial('Get camera must retrieve an specific camera in repository, exception on not found', async (t) => {
+  const wantedCamera = 'unknownCamera'
+  const exception = await t.throws(retrieveCamera(wantedCamera), CameraNotFoundException)
+  t.is(exception.message, `Cannot find camera ${wantedCamera}`)
+})
 
 test.serial('Save camera creation fields', async (t) => {
 
   await saveCamera(door1Camera)
   await saveCamera(livingRoomCamera)
   const result = await retrieveCamera(livingRoomCamera.name)
-  t.true(result.uuid.length === 36)
   t.is(result.name, livingRoomCamera.name)
   t.is(result.host, livingRoomCamera.host)
   t.is(result.user, livingRoomCamera.user)
   t.is(result.password, livingRoomCamera.password)
+  assertUuid(t, result.uuid)
   assertNearDate(t, new Date(result.createdAt), new Date(), 1000)
+})
+
+test.serial('Save same camera twice must be exception due to duplicated name', async (t) => {
+  await saveCamera(livingRoomCamera)
+  const exception = await t.throws(saveCamera(livingRoomCamera), CameraDuplicatedException)
+  t.is(exception.message, `Camera name ${livingRoomCamera.name} is duplicated`)
 })
 
 test.serial('Must return a listing with all cameras', async (t) => {
@@ -39,4 +53,27 @@ test.serial('Must return a listing with all cameras', async (t) => {
   const result = await getCameraListing()
   t.is(uuid1, result[0].uuid)
   t.is(uuid2, result[1].uuid)
+})
+
+test.serial('Must remove specific camera', async (t) => {
+  await saveCamera(door1Camera)
+  await saveCamera(livingRoomCamera)
+  await deleteCamera(livingRoomCamera.name)
+  const result = await getCameraListing()
+  t.is(1, result.length)
+  t.is(door1Camera.name, result[0].name)
+})
+
+test.serial('Must remove specific camera, exception on not found', async (t) => {
+  const wantedCamera = 'unknownCamera'
+  const exception = await t.throws(deleteCamera(wantedCamera), CameraNotFoundException)
+  t.is(exception.message, `Cannot find camera ${wantedCamera}`)
+})
+
+test.serial('Must remove all cameras', async (t) => {
+  await saveCamera(door1Camera)
+  await saveCamera(livingRoomCamera)
+  await deleteCameras()
+  const result = await getCameraListing()
+  t.is(0, result.length)
 })
